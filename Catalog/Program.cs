dfcb55ddaacbad9b7f62726fce4341cc341769a5
Prabashi <prabhashi.mm@gmail.com>
2024-01-18 16:50:00 +0800

    Persisting the entities with MongoDB hosted in Docker locally

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -1,11 +1,30 @@
 ﻿using Catalog.Repositories;
+using Catalog.Settings;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Serialize Guid and DateTimeOffset to obtain the desired format when written to the DB
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+//Register MongoDB Client
+builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
+{
+    var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    return new MongoClient(settings.ConnectionString);
+});
+
+//builder.Services.Configure<MongoDbSettings>(
+//    builder.Configuration.GetSection("MongoDbSettings"));
+
 // Register the dependencies
-builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
+builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
